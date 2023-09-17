d8a16da6392636a309d17fac6da2b41ee08fdd09
Hemant1224 <66623321+Hemant1224@users.noreply.github.com>
2023-09-17 11:42:58 +0530

    Create prime_factor.py

---
+++ 08. Prime factorization/prime_factor.py
@@ -0,0 +1,18 @@
+def isPrime(x):
+    for i in range (2,x):
+        if x % i ==0:
+            return False
+    return True
+
+
+def primeFactors(n):
+    for i in range(2, n+1):
+        if isPrime(i):
+            x = i
+            while(n % x == 0):
+                print(i)
+                x = x * i
+
+if __name__=="__main__":
+    a = int(input("Enter a number : "))
+    print(primeFactors(a))
